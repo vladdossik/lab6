76463ba6392d3d8ac0a70b93aa1876f74ceef193
Vlad <vladdosiik7540@gmail.com>
2020-04-27 17:02:57 +0300

    start

--- app/src/main/java/com/example/lab6/MainActivity.java
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button backend_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        backend_button = (Button) findViewById(R.id.back_end);
+        backend_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,backend.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/lab6/backend.java
+++ app/src/main/java/com/example/lab6/backend.java
@@ -2,13 +2,24 @@ package com.example.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class backend extends AppCompatActivity {
-
+Button storefrontbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_backend);
+        storefrontbutton=(Button)findViewById(R.id.store_front);
+        storefrontbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(backend.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
